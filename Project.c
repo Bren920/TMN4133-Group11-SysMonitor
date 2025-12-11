fde68554817a345a69bf3893bb08bcedac13abcd
mingzin02 <mingzin02@gmail.com>
2025-12-11 16:38:00 +0800

    Updated Project.c with How AI help

--- Project.c
+++ Project.c
@@ -3,5 +3,6 @@
 int main() {
     printf("Hello, World! Clarence love mingzin so much have a good time! \n");
     printf("How to do project? \n");
+    printf("How do AI assists in our project?\n");
     return 0;
 } 
